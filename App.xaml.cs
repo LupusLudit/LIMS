dc6e29634926cd0e20f536519578a7de704bc02f
LupusLudit <michal.bielina@centrum.cz>
2025-11-23 12:32:10 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LIMS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
